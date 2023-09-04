51468d29e3e75ff333f1619b907fedcaca07cbb1
Chanmeet <chanmeetsinghsahni@gmail.com>
2023-09-04 23:06:34 +0530

    aman sex sux

--- backend/AuthPortal/admin.py
+++ backend/AuthPortal/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models.user_model import User
+from .models.student_model import Student
 
+class UserAdmin(admin.ModelAdmin):
+    list_display=('user_id', 'name')
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('student_id', 'name')
+admin.site.register(User,UserAdmin)
+admin.site.register(Student, StudentAdmin)
 # Register your models here.
