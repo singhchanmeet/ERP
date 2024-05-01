7ac0bc3616c19aab41fbcda9ba801bb19ca0fe26
Ishaan Minocha <minochaishaan2003@gmail.com>
2024-05-02 02:42:29 +0530

    setup for work

--- backend/placement/views.py
+++ backend/placement/views.py
@@ -5,13 +5,8 @@ from .serializers  import *
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
-# <<<<<<< HEAD
-from rest_framework.authentication import SessionAuthentication
-# =======
 # from rest_framework.authentication import BasicAuthentication
-# >>>>>>> e1a95461e4b99065c19c192056fad219777692d5
 from django.utils import timezone
-
 
 
 # Create your views here.
@@ -19,19 +14,10 @@ from django.utils import timezone
 class AllPlacementRecords(ModelViewSet):
     authentication_classes =[SessionAuthentication]
     permission_classes =[ WriteByPlacement ]
-# <<<<<<< HEAD
-    authentication_classes =[SessionAuthentication]
-    permission_classes =[ WriteByPlacement ]
-# =======
     # authentication_classes =[BasicAuthentication]
     # permission_classes =[IsAuthenticated, WriteByPlacement ]
-    # permission_classes =[IsAuthenticated, WriteByPlacement ]
-# >>>>>>> e1a95461e4b99065c19c192056fad219777692d5
     serializer_class = PlacementSerializer
     queryset = Placements.objects.all()
-
-
-
 
 class ViewAnnouncement(ModelViewSet):
     # authentication_classes = [BasicAuthentication]
@@ -50,7 +36,6 @@ class ViewAnnouncement(ModelViewSet):
 
 class PastPlacementRecords(ModelViewSet):
     # authentication_classes = [BasicAuthentication]
-
     authentication_classes =[SessionAuthentication]
     permission_classes = [ WriteByPlacement]
     # permission_classes = [IsAuthenticated, WriteByPlacement]
@@ -61,11 +46,8 @@ class PastPlacementRecords(ModelViewSet):
     
 class ActivePlacementRecords(ModelViewSet):
     # authentication_classes = [BasicAuthentication]
-# <<<<<<< HEAD
     permission_classes = [ WriteByPlacement]
     authentication_classes =[SessionAuthentication]
-# =======
     # permission_classes = [IsAuthenticated, WriteByPlacement]
-# >>>>>>> e1a95461e4b99065c19c192056fad219777692d5
     serializer_class = PlacementSerializer
     queryset = Placements.objects.filter(date__gt=timezone.now()) 
